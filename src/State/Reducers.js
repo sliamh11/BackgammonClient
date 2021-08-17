42e8e24e552aaba93f201f2c60358098011928e0
User <User@DESKTOP-CT4MMP5>
2021-08-17 15:14:40 +0300

    Updated urls to production

--- src/State/Reducers.js
+++ src/State/Reducers.js
@@ -2,8 +2,7 @@ import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
 import io from 'socket.io-client';
 
-const socket_url = "http://localhost:8080";
-// const socket_url = "https://backgammonserver.azurewebsites.net";
+const socket_url = "https://backgammonserver.azurewebsites.net";
 const socket = io(socket_url, { transports: ['websocket', 'polling', 'flashsocket'] });
 
 const userStateReducer = (state = null, action) => {
@@ -20,17 +19,10 @@ const socketReducer = () => {
 const apiReducer = () => {
     return {
         chat: socket_url,
-        auth: "http://localhost:8080/api/auth",
-        users: "http://localhost:8080/api/users"
+        auth: "https://backgammonserver.azurewebsites.net/api/auth",
+        users: "https://backgammonserver.azurewebsites.net/api/users"
     }
 }
-// const apiReducer = () => {
-//     return {
-//         chat: socket_url,
-//         auth: "https://backgammonserver.azurewebsites.net/api/auth",
-//         users: "https://backgammonserver.azurewebsites.net/api/users"
-//     }
-// }
 
 const rootReducer = (history) => combineReducers({
     router: connectRouter(history),
